0146779aca09d7e504887f139413a25653b793ff
Josh T <slackerscpt@gmail.com>
2021-10-24 09:51:11 -0700

    Learning statistics

--- bowling-stats.py
+++ bowling-stats.py
@@ -1,24 +1,23 @@
-game_file = open('csvData\\Tuesday-2018-Stats.csv', 'r')
-
-#for contents in game_file:
-#    print (contents)
-
-
-print ("--LINE 1--")
-line1 = game_file.readline()
-print (line1)
-print ("--END--")
-
-for contents in game_file:
-    print (contents)
-game_file.close()
-
-
-
-
 import csv
-with open('csvData\\Tuesday-2018-Stats.csv', newline='',encoding = 'utf-8-sig' ) as csvfile:
+import statistics
+
+games = []
+with open('csvData\\Tuesday-2019-Stats.csv', newline='',encoding = 'utf-8-sig' ) as csvfile:
     reader = csv.DictReader(csvfile)
     for row in reader:
         #print(row['first_name'], row['last_name'])
         print(row['Week'],row['Gm1'], row['Gm2'], row['Gm3'], row['SS'])
+        if (int(row['SS']) > 0):
+            games.append(int(row['Gm1']))
+            games.append(int(row['Gm2']))
+            games.append(int(row['Gm3']))
+        
+
+csvfile.close()
+games.sort()
+print (games)
+print (sum(games))
+print (len(games))
+print (sum(games)/len(games))
+print (statistics.median(games))
+print (statistics.mean(games))
